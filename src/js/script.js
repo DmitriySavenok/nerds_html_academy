7fd804a48260afd8afaa26e9971b6965c105869a
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2021-11-05 08:18:46 +0300

    add button to top

--- src/js/script.js
+++ src/js/script.js
@@ -25,3 +25,18 @@ $(document).ready(function(){
         draggable:false
     });
 });
+
+$(function() {
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+        $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    
+    $('#toTop').click(function() { 
+        $('body,html').animate({scrollTop:0},800);
+    });
+});
