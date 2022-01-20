cf492532cb073e16c319e7914f064690b1c98c65
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-01-20 16:28:13 +0300

    оптимизировал функцию открывающую меню

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,15 +1,14 @@
 function headerMenuToggle() {
   var headerMenu = document.getElementById("header__menu");
   var headerMenuButton = document.getElementById("header__button-toggle");
-  var menuOpen = headerMenuButton.classList.item(1);
-
+  var menuOpen = headerMenuButton.classList.contains("menu-icon");
 
   headerMenu.classList.toggle("open-menu");
 
-  if(menuOpen === "menu-icon") {
+  if(menuOpen == true) {
     headerMenuButton.classList.remove("menu-icon");
     headerMenuButton.classList.add("close-icon");
-  } if(menuOpen === "close-icon") {
+  } if(menuOpen == false) {
     headerMenuButton.classList.remove("close-icon");
     headerMenuButton.classList.add("menu-icon");
   }
