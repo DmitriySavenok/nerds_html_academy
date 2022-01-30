5810808ce811fa44e9b6eb6083bfddc6d1199b34
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-01-30 16:49:37 +0300

    feedback modal window markup

---
+++ src/blocks/feedback-modal-window/feedback-modal-window.js
@@ -0,0 +1,11 @@
+function openFeedbackModal() {
+  var feedbackModal = document.getElementById("feedback-modal")
+
+  feedbackModal.classList.remove("close-window")
+}
+
+function closeFeedbackModal() {
+  var feedbackModal = document.getElementById("feedback-modal")
+
+  feedbackModal.classList.add("close-window")
+}
