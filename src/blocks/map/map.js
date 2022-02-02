efa1961b2f6a06c18158cb1399b036fe31a0d310
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-02-02 15:58:50 +0300

    adaptation footer for mobile screen

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -1,14 +1,19 @@
 var map;
   DG.then(function () {
+    if (window.innerWidth >= 650) {
       map = DG.map('map', {
-          center: [59.939014, 30.320971],
-          zoom: 17
-      });
-      var myIcon = DG.icon({
-        iconUrl: 'img/icons/map-marker.png',
-        iconRetinaUrl: 'img/icons/map-marker.png',
-        iconSize: [231, 190],
-        iconAnchor: [50, 190]
-      });
-      DG.marker([59.938635, 30.323118], {icon: myIcon}).addTo(map);
+      center: [59.939014, 30.320971],
+      zoom: 17});
+    } else {
+      map = DG.map('map', {
+      center: [59.939352, 30.323414],
+      zoom: 16});
+    }
+    var myIcon = DG.icon({
+      iconUrl: 'img/icons/map-marker.png',
+      iconRetinaUrl: 'img/icons/map-marker.png',
+      iconSize: [231, 190],
+      iconAnchor: [50, 190]
     });
+      DG.marker([59.938635, 30.323118], {icon: myIcon}).addTo(map);
+  });
